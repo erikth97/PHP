9003a157e41276f182e8ed9ec81d96b905408b92
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-04-03 10:30:09 -0600

    Create ejercicio2_sesion3.php

---
+++ ejercicio2_sesion3.php
@@ -0,0 +1,10 @@
+<?php
+//ejercicio cronometro con ciclo for
+for ($h=0; $h <=12; $h++) { 
+for ($m=0; $m <=59; $m++) { 
+    for ($s=0; $s <= 59; $s++) { 
+        echo "$h: horas con $m:minutos y $s:segundos<br>";
+    }
+}
+}
+?>
