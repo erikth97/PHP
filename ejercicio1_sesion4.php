95628bd1c574b053331118815641313735a66a66
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-04-04 09:34:31 -0600

    Create ejercicio1_sesion4.php

---
+++ ejercicio1_sesion4.php
@@ -0,0 +1,11 @@
+<?php
+//PROGRAMACION FUNCIONAL
+//FUNCIONES EN PHP
+//Es una pieza o bloque de codigo reutilizable que realiza una accion especifica, toma la entrada del usuario en forma de parametro, realiza ciertas acciones de salida. Pueden devolver valores cuando se llaman o realizar una operacion sin devolver ningun valor. En php tenemos mas de 700 funciones integradas.
+//Para que utilizar funciones?
+//- Mejor organizacion del codigo: Las funciones nor permite agrupar bloques de codigo relacionados que realizan una tarea especifica juntos.
+//- Reutilizacion: una ves definida la funcion puede ser llamada por varios scripts en nuestros archivos php, en pocas palabras ahorra tiempo llamando a la funcion o conectarlo a una base de datos.
+//- Mantenimiento sencillo.
+
+//Funciones Integradas: Son funciones predefinidas en Php que existen en el paquete de instalacion, son las que hacen que php sea un lenguaje de scripting. se pueden clasificar en: funciones de cadena, numericas, de fecha. otro tipo de funciones van con referencias al array.
+?>
