1cc453387484d9f134aa91cd902fe95d345abad0
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-04-04 09:31:26 -0600

    Create ejercicio6_sesion3.php

---
+++ ejercicio6_sesion3.php
@@ -0,0 +1,10 @@
+<?php
+//DO-WHILE
+//Hace referencia a "hacer mientras", ejecutamos un codigo al menos una ves antes de que forme parte del bucle, porque la condicion esta despues. 
+
+$i = 10;
+do {
+    echo "numeros: $i<br>";
+    $i++;
+} while ($i < 10);
+?>
