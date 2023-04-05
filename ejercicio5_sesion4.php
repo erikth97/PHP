a645d8d130821160a28e31a021e338ff937d3250
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-04-05 10:58:22 -0600

    Create ejercicio5_sesion4.php

---
+++ ejercicio5_sesion4.php
@@ -0,0 +1,9 @@
+<?php
+//Parametros de una funcion en php
+// Nuestras funciones seran mas interesantes si les damos algunos parametros. Al darle variables podemos usar siempre el mismo codigo pero con algunas varo
+function prueba($variable1, $variable2){
+    return $variable1.$variable2;
+}
+echo prueba(variable1:1,variable2:9);
+
+?>
