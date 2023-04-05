56c407dc440e038faa22c644c19585fd6e057dff
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-04-05 11:03:01 -0600

    Create ejercicio9_sesion4.php

---
+++ ejercicio9_sesion4.php
@@ -0,0 +1,12 @@
+<?php
+//Parametros
+function nombres($n1,$n2,$n3,$n4){
+    return $n1.$n2.$n3.$n4;
+}
+
+$listas_nombres = nombres(
+    n1: "pedro ",
+    n2: "ana ",
+    n3: "jose ",
+    n4: "maria "
+);
