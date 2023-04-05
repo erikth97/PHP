dd68c18a8329411c27dd613a385019c20178143a
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-04-05 10:57:57 -0600

    Create ejercicio4_sesion4.php

---
+++ ejercicio4_sesion4.php
@@ -0,0 +1,10 @@
+<?php
+//suma
+// es importante colocar retunr par auq e nos de una respuesta de la funcion.
+function suma($n1,$n2,$n3){
+    $sum = $n1 + $n2 + $n3;
+    echo $sum;
+    return;
+}
+suma(1,2,3);
+?>
